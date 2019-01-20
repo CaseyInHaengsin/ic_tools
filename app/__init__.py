3c257c2b8a754238cf08e812a9fa8c0a0b3800b0
Casey Rowley <casey.rowley89@gmail.com>
2019-01-19 17:46:57 -0700

    trying to adjust so that bootstrap is included

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from app.database import global_init as mongo_setup
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mongo_setup()
 login = LoginManager(app)
-
+bootstrap = Bootstrap(app)
 
 from app import routes
 
